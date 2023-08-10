9c5c5a2cfcacad6a01ebc90c2d44b48b85508a6c
escottmcgheeuhc <e.scott.mcghee.uhc@gmail.com>
2023-08-09 20:03:57 -0500

    Fixed psremote stage 1 BOF

--- KIT/PSremote/psremote_bof.s1.py
+++ KIT/PSremote/psremote_bof.s1.py
@@ -1,16 +1,27 @@
+from typing import List, Tuple
+
 from outflank_stage1.task.base_bof_task import BaseBOFTask
+from outflank_stage1.task.enums import BOFArgumentEncoding
+from outflank_stage1.task.tasks import DownloadTask
 
 
 class PSRemoteBOF(BaseBOFTask):
     def __init__(self):
         super().__init__("psremote")
 
-        self.parser.add_argument("remotehost", help="FQDN or IP remote host.")
+        self.parser.description = (
+            "List all running processes on a remote host."
+        )
+        self.parser.epilog = "Usage: psremote <remotehost>"
 
-        self.parser.description = "List all running processes on a remote host."
+        self.parser.add_argument(
+            "remotehost",
+            help=f"FQDN or IP Address of remote host"
+        )
 
-        self.parser.epilog = "Example usage:\n  - psremote dc-2.worldcom.lab\n  - psremote 10.0.10.10\n"
-
-    def _encode_arguments_bof(self, arguments: List[str]) -> List[Tuple[BOFArgumentEncoding, str]]:
+    def _encode_arguments_bof(
+        self, arguments: List[str]
+    ) -> List[Tuple[BOFArgumentEncoding, str]]:
         parser_arguments = self.parser.parse_args(arguments)
+
         return [(BOFArgumentEncoding.STR, parser_arguments.remotehost)]
