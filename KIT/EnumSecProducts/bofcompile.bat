67393598e6eda71ce61fc7acc37f8c8e84a513fd
smcghee <e.scott.mcghee.uhc@gmail.com>
2023-09-08 16:13:30 -0400

    Compiled new and old enumsecproducts

--- KIT/EnumSecProducts/bofcompile.bat
+++ KIT/EnumSecProducts/bofcompile.bat
@@ -4,4 +4,4 @@ cl.exe /nologo /c /Od /MT /W0 /GS- /Tc enumsecproducts.c
 move /y enumsecproducts.obj enumsecproducts.o
 
 cl.exe /nologo /c /Od /MT /W0 /GS- /Tc enumsecproducts_new.c
-move /y enumsecproducts.obj enumsecproducts_new.o
+move /y enumsecproducts_new.obj enumsecproducts_new.o
