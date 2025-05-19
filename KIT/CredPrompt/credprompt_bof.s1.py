2b6f562bd86b009e7e3c56f1ac9d88e222fae20d
smcghee <e.scott.mcghee.uhc@gmail.com>
2025-05-19 12:16:29 -0400

    Added credprompt to stage1

---
+++ KIT/CredPrompt/credprompt_bof.s1.py
@@ -0,0 +1,32 @@
+from typing import List, Tuple
+
+from outflank_stage1.task.base_bof_task import BaseBOFTask
+from outflank_stage1.task.enums import BOFArgumentEncoding
+
+
+
+class CredPromptBof(BaseBOFTask):
+    def __init__(self):
+        super().__init__("credprompt")
+
+        self.parser.description = (
+            "Prompt the user for their credentials. Specify the title and message of the popup box and also how long it'll run for."
+        )
+
+        self.parser.add_argument("title", help="Title of the prompt popup.", required=True)
+        self.parser.add_argument("message", help="Message of the prompt popup.", required=True)
+        self.parser.add_argument("timer", type=int, help="Timeout in seconds.", required=True)
+
+    def _encode_arguments_bof(
+        self, arguments: List[str]
+    ) -> List[Tuple[BOFArgumentEncoding, str]]:
+        parser_arguments = self.parser.parse_args(arguments)
+        title = parser_arguments.title
+        message = parser_arguments.message
+        timer = int(parser_arguments.timer)
+
+        return [
+            (BOFArgumentEncoding.WSTR, title),
+            (BOFArgumentEncoding.WSTR, message)
+            (BOFArgumentEncoding.INT, timer)
+        ]
